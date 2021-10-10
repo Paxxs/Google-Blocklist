f85b8dbd754df82c7942cba1d00d8b3f8ecffc3a
Paxxs <10364775+Paxxs@users.noreply.github.com>
2021-10-10 23:21:37 +0800

    readNetworkFile

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 )
 
@@ -13,6 +14,11 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(subscription)
+	networkSubscription, err := readNetworkFile("https://raw.githubusercontent.com/cobaltdisco/Google-Chinese-Results-Blocklist/master/uBlacklist_subscription.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(networkSubscription)
 }
 
 func readFile(path string) ([]string, error) {
@@ -22,6 +28,16 @@ func readFile(path string) ([]string, error) {
 	}
 	defer file.Close()
 	lines, err := ioToArr(file)
+	return lines, err
+}
+
+func readNetworkFile(path string) ([]string, error) {
+	resp, err := http.Get(path)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("response: ", resp.Status)
+	lines, err := ioToArr(resp.Body)
 	return lines, err
 }
 
